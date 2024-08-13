a407f19a9c589011296319821f43ac9e1f69d7a5
Claire Fuenning <cfuenning1@gmail.com>
2024-08-13 17:42:00 -0500

    uploading test_tele file

---
+++ test_television.py
@@ -0,0 +1,29 @@
+import unittest
+import television
+
+class TestTelevision(unittest.TestCase):
+
+    def test_init(self):
+        pass
+
+    def test_power(self):
+        pass
+
+    def test_mute(self):
+        pass
+
+    def test_channel_up(self):
+        pass
+
+    def test_channel_down(self):
+        pass
+
+    def test_volume_up(self):
+        pass
+
+    def test_volume_down(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
